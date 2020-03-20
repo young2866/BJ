eba69b0e4e375ddf2b29df2c6ae1e9673e76aeab
young2866 <62212610+young2866@users.noreply.github.com>
2020-03-20 23:45:25 +0900

    백준 2753

---
+++ BJ2753/BJ2753/2753.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+
+	scanf("%d", &N);
+
+	if ((N % 4 == 0) && (N % 100 != 0))
+		printf("1\n");
+	else if (N % 400 == 0)
+		printf("1\n");
+	else
+		printf("0");
+
+	return 0;
+
+}
